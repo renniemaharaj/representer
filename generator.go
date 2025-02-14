8bea45bc0c3fe7438d661833ecaa225f971a81e4
Rennie Maharaj <rvesprey@gmail.com>
2025-02-14 10:50:15 -0400

    Adjusted prompt selector

--- generator.go
+++ generator.go
@@ -31,7 +31,7 @@ func GetInstructions() string {
     				<li>Properly interpret Tailwind's bracket notation, escaped values, and arbitrary values:</li>  
     			<ul>  
         			<li>Convert <code>left-[50%]</code> → <code>left: 50%;</code></li> 
-					<li>Translate selectors with escaped values: <code>left-[50%]</code> → <code>.left-\[50\%\]</code></li>
+					<li>Translate selectors with escaped values: <code>left-[50%]</code> → <code>.left-\[50\%\]</code> (NOTICE ONE LEVEL OF ESCAPE | DOUBLE ESCAPING '//' WILL BREAK CSS SYNTAX) </li>
 					<li>DO NOT DOUBLE ESCAPE IN SELECTORS!! DO NOT DOUBLE ESCAPE!! .translate-x-\\[-50\\%\\] <- IS NOT VALID CSS, SHOULD BE -> .translate-x-\[-50\%\]<li>
 					<li>Translate all selectors appropriately as the above example</li>
         			<li>Convert <code>h-[calc(100vh-4rem)]</code> → <code>height: calc(100vh - 4rem);</code></li>  
